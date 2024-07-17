8284d1715069bb4dde6f1ebd0d2e711f1e69b3c0
Isaacxavierlee <reivax.caasi@gmail.com>
2024-07-17 17:19:56 +0800

    wto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,21 @@
 import { useLocation } from 'react-router-dom';
-
+import { useState } from 'react';
 import { brainwave } from '../assets'; // Adjust the path and file extension as necessary
 import { navigation } from '../constants';
 import Button from './Button';
-
+import MenuSvg from '../assets/svg/MenuSvg';
+import { HamburgerMenu } from './design/Header';
 
 const Header = () => {
-
-    const pathname = useLocation();
+  const { pathname } = useLocation();
+  const [openNavigation, setOpenNavigation] = useState(false);
 
   return (
-    <div className="fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm bg-n-8 backdrop-blur-sm">
+    <div
+      className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+        openNavigation ? 'bg-n-8' : 'bg-n-8/90 backdrop-blur-sm'
+      }`}
+    >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
           <img src={brainwave} width={190} height={40} alt="Brainwave" />
@@ -20,19 +25,18 @@ const Header = () => {
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((item) => (
               <a
-              key={item.id}
-              href={item.url}
-              //onClick={handleClick}
-              className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
-                item.onlyMobile ? "lg:hidden" : ""
-              } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
-                item.url === pathname.hash
-                  ? "z-2 lg:text-n-1"
-                  : "lg:text-n-1/50"
-              } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
-            >
-              {item.title}
-            </a>
+                key={item.id}
+                href={item.url}
+                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
+                  item.onlyMobile ? 'lg:hidden' : ''
+                } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
+                  item.url === pathname.hash
+                    ? 'z-2 lg:text-n-1'
+                    : 'lg:text-n-1/50'
+                } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
+              >
+                {item.title}
+              </a>
             ))}
           </div>    
         </nav>
@@ -46,12 +50,9 @@ const Header = () => {
         <Button className="hidden lg:flex" href="#login">
           Sign in
         </Button>
-
-
-
       </div>
     </div>
   );
-}
+};
 
 export default Header;
