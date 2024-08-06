f7c089e361f14218ea39ef8ef6ac7fb55753257e
Isaacxavierlee <reivax.caasi@gmail.com>
2024-08-06 16:10:14 +0800

    sup soup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { useLocation } from "react-router-dom";
 import { disablePageScroll, enablePageScroll } from "scroll-lock";
 
-import { brainwave } from "../assets";
+import brainwave from "../assets/thunder.png"; // Correctly import the image
 import { navigation } from "../constants";
 import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
@@ -31,13 +31,14 @@ const Header = () => {
 
   return (
     <div
-      className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+      className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
         openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
       }`}
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-        <a className="block w-[12rem] xl:mr-8" href="#hero">
-          <img src={brainwave} width={190} height={40} alt="Brainwave" />
+        <a className="flex items-center w-[12rem] xl:mr-8" href="#hero">
+          <img src={brainwave} width={40} height={20} alt="MindPulse" /> {/* Use the imported image */}
+          <span className="ml-2 text-xl font-bold text-white">MindPulse</span> {/* Add the title */}
         </a>
 
         <nav
